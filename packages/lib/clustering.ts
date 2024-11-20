26dd413b0eb4ebf686f7c8ee2dee23e442a5d662
satchelspencer <satchelspencer@gmail.com>
2024-11-19 18:24:22 -0700

    simplify clustering

--- packages/lib/clustering.ts
+++ packages/lib/clustering.ts
@@ -25,34 +25,18 @@ export function clusterNodes(elementId: string, elements: t.IdMap<t.Element>) {
 
   const clusters: string[][][][] = []
   for (const adjList of adjLists) {
-    const adjMatrix: number[][] = []
-
-    const adjListKeys = Object.keys(adjList)
-    for (let i = 0; i < adjListKeys.length; i++) {
-      adjMatrix[i] = []
-      const a = adjListKeys[i]
-      for (let j = 0; j < adjListKeys.length; j++) {
-        const b = adjListKeys[j]
-        if (a === b) {
-          adjMatrix[i][j] = 1
-        } else {
-          const common = _.intersection(adjList[a], adjList[b]).length
-          adjMatrix[i][j] = common
-        }
-      }
-    }
-
-    const levels = cluster({
-      input: adjListKeys,
-      distance(a, b) {
-        return 1 / adjMatrix[adjListKeys.indexOf(a)][adjListKeys.indexOf(b)]
-      },
-      linkage(distances) {
-        // var sum = distances.reduce((a, b) => a + b, 0) // Sum of all distances
-        // return sum / distances.length // Average distance
-        return Math.min.apply(null, distances)
-      },
-    })
+    const adjListKeys = Object.keys(adjList),
+      levels = cluster({
+        input: adjListKeys,
+        distance(a, b) {
+          return 1 / _.intersection(adjList[a], adjList[b]).length
+        },
+        linkage(distances) {
+          // var sum = distances.reduce((a, b) => a + b, 0) // Sum of all distances
+          // return sum / distances.length // Average distance
+          return Math.min.apply(null, distances)
+        },
+      })
 
     clusters.push(
       levels.map((level) => level.clusters.map((c) => c.map((id) => adjListKeys[id])))
