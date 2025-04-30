61dcd4880a8088daf96f3732bc6c3107f372a41d
satchelspencer <satchelspencer@gmail.com>
2025-04-29 22:48:24 -0600

    dev script runs tts at the same time

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: { port: 8177 },
   plugins: [react()],
   base: './',
-  build: { assetsDir: '' },
+  build: { assetsDir: '', outDir: '../dist/', emptyOutDir: true },
   worker: {
     format: 'es',
   },
